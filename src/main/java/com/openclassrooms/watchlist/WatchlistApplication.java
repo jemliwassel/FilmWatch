97c7e187cb0f0a5637acc1a83d5801290d551dfa
jemliwassel <wassel.jemli@etudiant-enit.utm.tn>
2024-02-08 23:54:55 +0100

    first commit

---
+++ src/main/java/com/openclassrooms/watchlist/WatchlistApplication.java
@@ -0,0 +1,15 @@
+package com.openclassrooms.watchlist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+public class WatchlistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WatchlistApplication.class, args);
+	}
+
+}
