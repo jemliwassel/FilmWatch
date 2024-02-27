7036a6c4eaf0bbc24f359cbf38f5adbed92a8b91
jemliwassel <wassel.jemli@etudiant-enit.utm.tn>
2024-02-27 12:32:17 +0100

    feat: add AppConfig class to use XML file for declaring the different beans

---
+++ src/main/java/com/openclassrooms/watchlist/config/AppConfig.java
@@ -0,0 +1,10 @@
+package com.openclassrooms.watchlist.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("app-config.xml")
+public class AppConfig {
+
+}
