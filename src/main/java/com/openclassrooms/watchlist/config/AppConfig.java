8b91317a85c86f29b6144780f785714d432155e7
jemliwassel <wassel.jemli@etudiant-enit.utm.tn>
2024-04-01 14:12:27 +0200

    feat: add AppConfig.java

--- src/main/java/com/openclassrooms/watchlist/config/AppConfig.java
+++ src/main/java/com/openclassrooms/watchlist/config/AppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.ImportResource;
 
 import com.openclassrooms.watchlist.repository.WatchlistRepository;
 import com.openclassrooms.watchlist.service.MovieRatingService;
+import com.openclassrooms.watchlist.service.MovieRatingServiceImpl;
 import com.openclassrooms.watchlist.service.WatchlistService;
 
 @Configuration
@@ -18,7 +19,7 @@ public class AppConfig {
 	
 	@Bean
 	public MovieRatingService movieRatingService() {
-		return new MovieRatingService();
+		return new MovieRatingServiceImpl();
 	}
 	
 	@Bean
